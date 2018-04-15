32458dca5753e57500cff394bcc4868ef17e9521
Chu Cheuk Kiu <calvinchu26@gmail.com>
2018-04-16 00:15:50 +0800

    uploading everything to github using makefile

---
+++ lab/lab8/square.h
@@ -0,0 +1,20 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include "common.h"
+
+class Square {
+	friend double area_sum(const Rectangle& r, const Square& s);
+	friend class TestSquare;
+public:
+	Square(double x, double y, double len) : x(x), y(y), len(len) {};
+
+private:
+	double area() const { return len * len; }
+
+	double x;
+	double y;
+	double len;
+};
+
+#endif
